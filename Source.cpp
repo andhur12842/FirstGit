a54e06bdf019a01d117827b6195785d799c583d4
den <a.gurman@ukr.net>
2022-10-03 13:05:14 +0300

    Initial commit. Created Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include "Libraries.h"
+#include <iomanip>
+//setlocale(LC_ALL, "ru");
+//srand(time(NULL));
+// SetConsoleCP(1251);
+// SetConsoleOutputCP(1251);
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+
+
+	retu
