86d3e48d1d4478fd3e3f29091409f84d8ff15354
den <a.gurman@ukr.net>
2022-10-03 14:19:08 +0300

    Added Struct Student and basic foo

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,71 @@
-#include "Libraries.h"
-#include <iomanip>
-//setlocale(LC_ALL, "ru");
-//srand(time(NULL));
-// SetConsoleCP(1251);
-// SetConsoleOutputCP(1251);
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+struct Student
+{
+	string name;
+	int age = 0;
+	int marks[100];
+	int marks_count = 0;
+};
 
+double getAverageMark(Student st)
+{
+	double averageMark = 0;
+	for (size_t i = 0; i < st.marks_count; i++)
+	{
+		averageMark += st.marks[i];
+	}
+	averageMark /= st.marks_count;
+	return averageMark;
+}
+
+void fillStudentByHand(Student &st)
+{
+	cout << "Enter name:" << endl;
+	cin >> st.name;
+	cout << "Enter age:" << endl;
+	cin >> st.age;
+	cout << "Enter count of marks:" << endl;
+	cin >> st.marks_count;
+	for (size_t i = 0; i < st.marks_count; i++)
+	{
+		cout << "Enter mark:" << endl;
+		cin >> st.marks[i];
+	}
+}
+
+void Print(Student &st)
+{
+	cout << st.name << endl;
+	cout << st.age << endl;
+	cout << st.marks_count << endl;
+	for (size_t i = 0; i < st.marks_count; i++)
+	{
+		cout << st.marks[i] << " ";
+	}
+	cout << endl;
+}
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
+	int countOfMarks = 2;
+	const int SIZE = 2;
+	Student group[SIZE];
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		fillStudentByHand(group[i]);
+	}
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		Print(group[i]);
+	}
+	
 
 
-
-	retu
+	return 0;
+}
